25a5bd228d65117f19183b27ec3bc19393b12f69
Axel Demborg <axel.demborg@gmail.com>
2026-04-19 21:01:51 +0200

    A square within a square?!

--- src/main.ts
+++ src/main.ts
@@ -16,16 +16,77 @@ context.configure({
   format: canvasFormat,
 });
 
+const vertices = new Float32Array([
+  -0.8, -0.8, 0.8, -0.8, 0.8, 0.8, -0.8, -0.8, 0.8, 0.8, -0.8, 0.8,
+]);
+
+const vertexBuffer = device.createBuffer({
+  label: "Cell vertices",
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+});
+
+device.queue.writeBuffer(vertexBuffer, 0, vertices);
+
+const vertexBufferLayout = {
+  arrayStride: 8,
+  attributes: [
+    {
+      format: "float32x2",
+      offset: 0,
+      shaderLocation: 0,
+    },
+  ],
+};
+
+const cellShaderModule = device.createShaderModule({
+  label: "Cell shader",
+  code: `
+  @vertex
+  fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+    return vec4f(pos, 0, 1);
+  }
+
+  @fragment
+  fn fragmentMain() -> @location(0) vec4f {
+    return vec4f(0.3, 0.3, 1, 1);
+  }
+  `,
+});
+
+const cellPipeline = device.createRenderPipeline({
+  label: "Cell pipeline",
+  layout: "auto",
+  vertex: {
+    module: cellShaderModule,
+    entryPoint: "vertexMain",
+    buffers: [vertexBufferLayout],
+  },
+  fragment: {
+    module: cellShaderModule,
+    entryPoint: "fragmentMain",
+    targets: [
+      {
+        format: canvasFormat,
+      },
+    ],
+  },
+});
+
 const encoder = device.createCommandEncoder();
 const pass = encoder.beginRenderPass({
   colorAttachments: [
     {
       view: context.getCurrentTexture().createView(),
       loadOp: "clear",
+      clearValue: { r: 0.3, g: 0, b: 0, a: 1 },
       storeOp: "store",
     },
   ],
 });
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2);
 pass.end();
 const commandBuffer = encoder.finish();
 device.queue.submit([commandBuffer]);
