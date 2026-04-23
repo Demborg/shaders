f7255d6a50f4b1a17d98db97e4e5a95f304031e4
Axel Demborg <axel.demborg@gmail.com>
2026-04-23 20:51:47 +0200

    Use some ping pong buffers for a shitty animation

--- src/main.ts
+++ src/main.ts
@@ -29,15 +29,26 @@ const uniformBuffer = device.createBuffer({
 device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
 
 const cellStateArray = new Uint32Array(GRID_SIZE * GRID_SIZE);
-const cellStateStorage = device.createBuffer({
-  label: "Cell State",
-  size: cellStateArray.byteLength,
-  usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-});
+const cellStateStorage = [
+  device.createBuffer({
+    label: "Cell State A",
+    size: cellStateArray.byteLength,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  }),
+  device.createBuffer({
+    label: "Cell State B",
+    size: cellStateArray.byteLength,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+  }),
+];
 for (let i = 0; i < cellStateArray.length; i += 3) {
   cellStateArray[i] = 1;
 }
-device.queue.writeBuffer(cellStateStorage, 0, cellStateArray);
+device.queue.writeBuffer(cellStateStorage[0], 0, cellStateArray);
+for (let i = 0; i < cellStateArray.length; i++) {
+  cellStateArray[i] = i % 2;
+}
+device.queue.writeBuffer(cellStateStorage[1], 0, cellStateArray);
 
 const vertices = new Float32Array([
   -0.8, -0.8, 0.8, -0.8, 0.8, 0.8, -0.8, -0.8, 0.8, 0.8, -0.8, 0.8,
@@ -84,36 +95,60 @@ const cellPipeline = device.createRenderPipeline({
   },
 });
 
-const bindGroup = device.createBindGroup({
-  label: "Cell render bind group",
-  layout: cellPipeline.getBindGroupLayout(0),
-  entries: [
-    {
-      binding: 0,
-      resource: { buffer: uniformBuffer },
-    },
-    {
-      binding: 1,
-      resource: { buffer: cellStateStorage },
-    },
-  ],
-});
+const bindGroup = [
+  device.createBindGroup({
+    label: "Cell render bind group A",
+    layout: cellPipeline.getBindGroupLayout(0),
+    entries: [
+      {
+        binding: 0,
+        resource: { buffer: uniformBuffer },
+      },
+      {
+        binding: 1,
+        resource: { buffer: cellStateStorage[0] },
+      },
+    ],
+  }),
+  device.createBindGroup({
+    label: "Cell render bind group B",
+    layout: cellPipeline.getBindGroupLayout(0),
+    entries: [
+      {
+        binding: 0,
+        resource: { buffer: uniformBuffer },
+      },
+      {
+        binding: 1,
+        resource: { buffer: cellStateStorage[1] },
+      },
+    ],
+  }),
+];
 
-const encoder = device.createCommandEncoder();
-const pass = encoder.beginRenderPass({
-  colorAttachments: [
-    {
-      view: context.getCurrentTexture().createView(),
-      loadOp: "clear",
-      clearValue: { r: 0.3, g: 0, b: 0, a: 1 },
-      storeOp: "store",
-    },
-  ],
-});
-pass.setPipeline(cellPipeline);
-pass.setVertexBuffer(0, vertexBuffer);
-pass.setBindGroup(0, bindGroup);
-pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE);
-pass.end();
-const commandBuffer = encoder.finish();
-device.queue.submit([commandBuffer]);
+let step = 0;
+const UPDATE_INTERVAL = 200;
+
+function update() {
+  step++;
+  const encoder = device.createCommandEncoder();
+  const pass = encoder.beginRenderPass({
+    colorAttachments: [
+      {
+        view: context.getCurrentTexture().createView(),
+        loadOp: "clear",
+        clearValue: { r: 0.3, g: 0, b: 0, a: 1 },
+        storeOp: "store",
+      },
+    ],
+  });
+  pass.setPipeline(cellPipeline);
+  pass.setVertexBuffer(0, vertexBuffer);
+  pass.setBindGroup(0, bindGroup[step % 2]);
+  pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE);
+  pass.end();
+  const commandBuffer = encoder.finish();
+  device.queue.submit([commandBuffer]);
+}
+
+setInterval(update, UPDATE_INTERVAL);
